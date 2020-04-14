bf8a36a5d7f667fdc7ed756575235c7d47c706c0
Felipe Beline Baravieira <felipe@fbeline.com>
2020-04-14 14:14:44 -0300

    pressed keys - WIP

--- source/app.d
+++ source/app.d
@@ -1,43 +1,64 @@
 import std.stdio;
 import std.string : toStringz, strip;
 import std.conv;
+import std.typecons;
 import fuzzyd.core;
 import deimos.ncurses.curses;
 
-int main()
-{
+string[] parseStdin() {
   string l;
   string[] lines;
   while ((l = stdin.readln()) !is null)
     lines ~= strip(l);
-  auto f = fuzzy(lines);
-  auto cmd = toStringz(f("cd")[0].value);
+  return lines;
+}
 
-  // immutable msg = toStringz("hello world");
+enum KeyType {SPECIAL, NORMAL, UNKOWN};
+
+struct Key {
+  KeyType type;
+  wint_t key;
+
+  void get() {
+    switch (get_wch(&key)) {
+      case KEY_CODE_YES:
+        type = KeyType.SPECIAL;
+        break;
+      case OK:
+        type = KeyType.NORMAL;
+        break;
+      default:
+        type = KeyType.UNKOWN;
+    }
+  }
+}
+
+
+int main() {
+  auto f = fuzzy(parseStdin());
 
   File tty = File("/dev/tty", "r+");
-	SCREEN* screen = newterm(null, tty.getFP, tty.getFP);
+  SCREEN* screen = newterm(null, tty.getFP, tty.getFP);
   screen.set_term;
   scope (exit)
     endwin();
   cbreak();
   noecho();
-	keypad(stdscr, true);
+  keypad(stdscr, true);
 
-	mvprintw(0, 0, toStringz("search: "));
+  mvprintw(0, 0, toStringz("search: "));
   refresh();
 
-  int key;
   string pattern;
-  while((key = getch()) != ERR) {
-    clear();
-    pattern ~= key;
-    mvprintw(10, 0, toStringz(to!string(key)));
+  auto key = Key();
+  do {
+    key.get();
+    if (key.type == KeyType.NORMAL) pattern ~= to!char(key.key);
+    // mvprintw(10, 0, toStringz(key.key.to!string));
     mvprintw(0, 0, toStringz("search: " ~ pattern));
     refresh();
-  }
+  } while(key.type != KeyType.UNKOWN);
 
   endwin();
-
   return 0;
 }
