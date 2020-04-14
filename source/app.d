f8103902edc9712a8d931647e73ca96e00cd2faf
Felipe Beline Baravieira <felipe@fbeline.com>
2020-04-14 02:08:48 -0300

    ncurses setup

--- source/app.d
+++ source/app.d
@@ -1,6 +1,33 @@
 import std.stdio;
+import std.string : toStringz, strip;
+import fuzzyd.core;
+import deimos.ncurses.curses;
 
-void main()
+int main()
 {
-	writeln("Edit source/app.d to start your project.");
+  string l;
+  string[] lines;
+  while ((l = stdin.readln()) !is null)
+    lines ~= strip(l);
+  auto f = fuzzy(lines);
+  auto cmd = toStringz(f("cd")[0].value);
+
+  // immutable msg = toStringz("hello world");
+
+  File tty = File("/dev/tty", "r+");
+	SCREEN* screen = newterm(null, tty.getFP, tty.getFP);
+  screen.set_term;
+  scope (exit)
+    endwin();
+  cbreak();
+  noecho();
+	keypad(stdscr, true);
+
+  printw(cmd);
+  refresh();
+
+  getch();
+  endwin();
+
+  return 0;
 }
