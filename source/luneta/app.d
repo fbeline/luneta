dab7c78baa3be35ce2cfdd619b5c74a98554718e
Felipe Beline Baravieira <felipe@fbeline.com>
2020-04-15 17:40:48 -0300

    format code with dfmt

---
+++ source/luneta/app.d
@@ -0,0 +1,83 @@
+import std.stdio;
+import std.string : toStringz, strip;
+import std.conv;
+import std.typecons;
+import std.algorithm;
+import std.array;
+import fuzzyd.core;
+import deimos.ncurses.curses;
+import luneta.printers;
+import luneta.keyboard;
+
+const MAX_PRINT = 20;
+const printFn[] printers = [
+    &printMatches, &printSelection, &printTotalMatches, &printCursor
+];
+
+string[] parseStdin()
+{
+    string l;
+    string[] lines;
+    while ((l = stdin.readln()) !is null)
+        lines ~= strip(l);
+    return lines;
+}
+
+alias loopFn = void delegate();
+loopFn loop(fuzzyFn fzy, ref string result)
+{
+    return delegate void() {
+        auto kp = KeyProcessor();
+        do
+        {
+            kp.getKey();
+            clear();
+            if (kp.terminate)
+            {
+                result = kp.getSelected;
+                break;
+            }
+            if (kp.dosearch)
+            {
+                kp.allMatches = fzy(kp.pattern);
+                kp.matches = kp.allMatches.filter!(m => m.score > 0).array();
+                kp.selected = MAX_PRINT - 1;
+            }
+            printMatches(kp);
+            printSelection(kp);
+            printTotalMatches(kp);
+            foreach (fn; printers)
+                fn(kp);
+            refresh();
+        }
+        while (kp.key.type != KeyType.UNKOWN);
+    };
+}
+
+void cursesInit(loopFn loop)
+{
+    File tty = File("/dev/tty", "r+");
+    SCREEN* screen = newterm(null, tty.getFP, tty.getFP);
+    screen.set_term;
+    scope (exit)
+        endwin();
+    cbreak;
+    noecho;
+    keypad(stdscr, true);
+
+    mvprintw(1, 0, toStringz("> "));
+    refresh();
+
+    loop();
+
+    endwin();
+}
+
+int main()
+{
+    auto fzy = fuzzy(parseStdin());
+    string result;
+    cursesInit(loop(fzy, result));
+    write(result);
+    return 0;
+}
---
+++ source/luneta/printers.d
@@ -0,0 +1,72 @@
+module luneta.printers;
+
+import std.conv;
+import std.algorithm;
+import std.string : toStringz, strip;
+import deimos.ncurses.curses;
+import luneta.keyboard;
+import fuzzyd.core;
+
+const MAX_PRINT = 20;
+alias printFn = void function(KeyProcessor);
+
+void printMatches(KeyProcessor kp)
+{
+
+    void printLine(int line, FuzzyResult m)
+    {
+        for (int i; i < m.value.length; i++)
+        {
+            if (m.matches.canFind(i))
+            {
+                attron(A_BOLD);
+                mvprintw(line, i + 2, toStringz(m.value[i].to!string));
+                attroff(A_BOLD);
+            }
+            else
+            {
+                mvprintw(line, i + 2, toStringz(m.value[i].to!string));
+            }
+        }
+    }
+
+    for (int i; i < min(MAX_PRINT, kp.matches.length); i++)
+    {
+        immutable int lineNumber = MAX_PRINT - i - 1;
+        if (lineNumber is kp.selected)
+        {
+            attron(A_REVERSE);
+            printLine(lineNumber, kp.matches[i]);
+            attroff(A_REVERSE);
+        }
+        else
+        {
+            printLine(lineNumber, kp.matches[i]);
+        }
+    }
+}
+
+void printSelection(KeyProcessor kp)
+{
+    attron(A_REVERSE);
+    immutable stopLine = max(0, MAX_PRINT - kp.matches.length);
+    for (int i = MAX_PRINT - 1; i >= stopLine; i--)
+        mvprintw(i, 0, toStringz(" "));
+    if (kp.matches.length > 0)
+        mvprintw(kp.selected, 0, toStringz("> "));
+    attroff(A_REVERSE);
+}
+
+void printTotalMatches(KeyProcessor kp)
+{
+    auto str = (to!string(kp.matches.length) ~ "/" ~ to!string(kp.allMatches.length)).toStringz;
+
+    attron(A_BOLD);
+    mvprintw(MAX_PRINT, 1, str);
+    attroff(A_BOLD);
+}
+
+void printCursor(KeyProcessor kp)
+{
+    mvprintw(MAX_PRINT + 1, 0, toStringz("> " ~ kp.pattern));
+}
