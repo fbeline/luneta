ea75aed6f6053aa14ad87e8357902513d0cc0cf4
Felipe Beline Baravieira <felipe@fbeline.com>
2020-05-02 15:58:45 -0300

    filter mode
    allowing user to pass arg --filter|f.
    The filter mode do not start the interactive mode and returns all the matches directly to the stdout

--- source/luneta/app.d
+++ source/luneta/app.d
@@ -8,10 +8,13 @@ import luneta.printers;
 import luneta.keyboard;
 import luneta.window;
 import luneta.opts;
+import std.algorithm;
+import std.container.binaryheap;
+import std.range;
 
 private:
 
-const string VERSION = "v0.5.0";
+const string VERSION = "v0.6.0";
 
 struct Result
 {
@@ -26,6 +29,17 @@ string[] parseStdin()
     while ((l = stdin.readln()) !is null)
         lines ~= strip(l);
     return lines;
+}
+
+Result filterMode(fuzzyFn fzy, long size, string pattern)
+{
+    auto fr = new FuzzyResult[size];
+    const total = fzy(pattern, fr);
+    return Result(
+        heapify!"a.score < b.score"(fr)
+            .take(total)
+            .map!(x => x.value)
+            .array);
 }
 
 void delegate() loop(fuzzyFn fzy, ulong dbsize, ref Result result)
@@ -61,7 +75,7 @@ int main(string[] args)
     auto helpInformation = getopt(args, std.getopt.config.passThrough,
             "version|v", "version", &_version,
             "query|q", "default query to be used upon startup", &luneta.opts.query,
-            "filter|f", "do not start interactive finder", &luneta.opts.filterMode,
+            "filter|f", "do not start interactive finder, e.g -f=\"pattern\"", &luneta.opts.filter,
             "height", "set the maximum window height (number of lines), e.g --height 25", &luneta.opts.height,
             "color", "color support, e.g --color=FALSE", &luneta.opts.colorSupport);
 
@@ -79,9 +93,13 @@ int main(string[] args)
     auto db = parseStdin();
     auto fzy = fuzzy(db);
     Result result = Result();
-    init(loop(fzy, db.length, result));
 
-    foreach(l; result.value)
+    if (luneta.opts.filter.empty)
+        init(loop(fzy, db.length, result));
+    else
+        result = filterMode(fzy, db.length, luneta.opts.filter);
+
+    foreach (l; result.value)
     {
         writeln(l);
     }
--- source/luneta/opts.d
+++ source/luneta/opts.d
@@ -3,4 +3,4 @@ module luneta.opts;
 static int height;  /// window height
 static bool colorSupport = true; /// colorized result
 static string query; /// default query to be used upon startup
-static bool filterMode; /// if true do not start interactive finder
+static string filter; /// if true do not start interactive finder
