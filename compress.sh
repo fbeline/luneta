ef3380b597f27686357823ef65bb04045eb07f31
Felipe Beline Baravieira <felipe@fbeline.com>
2020-05-03 13:26:06 -0300

    travis deploy binaries - WIP

--- compress.sh
+++ compress.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+mkdir -p releases
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 dub build -b release --compiler ldc2
-tar -czvf luneta-linux-amd64.tar.gz ./luneta
+tar -czvf releases/luneta-$machine.tar.gz ./luneta
