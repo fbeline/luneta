172bd3c87b9a2efe089bfe998347d313fbe2e62f
Felipe Beline Baravieira <felipe@fbeline.com>
2020-05-04 19:01:34 -0300

    remove old examples

--- examples/df.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# shows available and used disk space on the Linux system.
-df -h | tail -n +2 | luneta | awk '{print $6}'
--- examples/git-branch-checkout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git branch | luneta | xargs git checkout
